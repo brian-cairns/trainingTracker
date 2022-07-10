1f644a331e3083983e0d2e8740798f4c7389ce08
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-07-10 13:06:21 -0400

    Still messing with cors

--- submitForm.js
+++ submitForm.js
@@ -90,7 +90,6 @@ async function submitForm(data, form) {
   console.log(document)
   fetch('https://pffm.azurewebsites.net/form', {
     method: "POST",
-    mode: 'cors',
     headers: {
       'Content-Type': 'application/JSON',
       "Access-Control-Allow-Origin" : "*"
